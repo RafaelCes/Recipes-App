d907896f03cd616974e91582c635973b1430913c
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-08-20 23:06:41 -0300

    deploy build

---
+++ build/precache-manifest.e98de48dfcbd72c94e9d6596f9a59acb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a891c0b74d2441b923e977d9800cdf7b",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "8fee12c72d163c38f4be",
+    "url": "/Recipe-App/static/css/2.9165716f.chunk.css"
+  },
+  {
+    "revision": "3a9cd8f6f6c15c5b1a9d",
+    "url": "/Recipe-App/static/css/main.72e71eb6.chunk.css"
+  },
+  {
+    "revision": "8fee12c72d163c38f4be",
+    "url": "/Recipe-App/static/js/2.8ce0ea9f.chunk.js"
+  },
+  {
+    "revision": "9353b4df7938eafd5f8e3a0cf5a68c12",
+    "url": "/Recipe-App/static/js/2.8ce0ea9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9cd8f6f6c15c5b1a9d",
+    "url": "/Recipe-App/static/js/main.6e263d2f.chunk.js"
+  },
+  {
+    "revision": "ac9687088435faf96c37",
+    "url": "/Recipe-App/static/js/runtime-main.95426c1a.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/Recipe-App/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "ce3404e677a420011de5d9093cc5131c",
+    "url": "/Recipe-App/static/media/drink.ce3404e6.jpg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/Recipe-App/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/Recipe-App/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b54995c6ac9cf8458434bbc723c56185",
+    "url": "/Recipe-App/static/media/food-wallpaper.b54995c6.jpg"
+  },
+  {
+    "revision": "9eaf0ee3e2362e8cc5ffd975cea4b597",
+    "url": "/Recipe-App/static/media/food.9eaf0ee3.jpeg"
+  },
+  {
+    "revision": "5945d0a15f9093d94e82f72844bb053e",
+    "url": "/Recipe-App/static/media/fruits.5945d0a1.jpg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/Recipe-App/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/Recipe-App/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/Recipe-App/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/Recipe-App/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/Recipe-App/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
